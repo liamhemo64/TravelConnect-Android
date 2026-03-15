e3ed8b4c1969476e1e9948e4260cb71c9955420a
annaeden1 <annaeden1@gmail.com>
2026-03-15 14:56:27 +0200

    move cloudinary config to different file and remove data from git

---
+++ app/src/main/java/com/idz/travelconnect/base/Config.kt
@@ -0,0 +1,7 @@
+package com.idz.travelconnect.base
+
+val cloudinaryConfig = mapOf(
+    "cloud_name" to "duutna6lt",
+    "api_key" to "646421153716863",
+    "api_secret" to "zbfAoiCiwRSW0_Hj39GzHerfnbU"
+)
