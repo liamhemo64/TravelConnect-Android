05bc2d3c56ca966df1a41aaa37a1881d10e9c74f
annaeden1 <annaeden1@gmail.com>
2026-03-17 19:03:29 +0200

    add commit item ui

---
+++ app/src/main/java/com/idz/travelconnect/features/postdetail/comment/CommentsAdapter.kt
@@ -0,0 +1,36 @@
+package com.idz.travelconnect.features.postdetail.comment
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.idz.travelconnect.databinding.ItemCommentBinding
+import com.idz.travelconnect.model.Comment
+import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CommentsAdapter(
+    private val currentUserId: String,
+    private val onDeleteClick: (Comment) -> Unit
+) : ListAdapter<Comment, CommentViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Comment>() {
+            override fun areItemsTheSame(oldItem: Comment, newItem: Comment) = oldItem.id == newItem.id
+            override fun areContentsTheSame(oldItem: Comment, newItem: Comment) = oldItem == newItem
+        }
+        private val timeFormat = SimpleDateFormat("MMM d, HH:mm", Locale.getDefault())
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
+        val binding = ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CommentViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
